b75797915c9a1c2a794ab64b7771fd8012aaf9fb
Aryan Deore <aryandeore98@gmail.com>
2026-07-05 21:34:13 -0400

    Initial Commit

---
+++ WatchLogs/WatchLogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLogsApp.swift
+//  WatchLogs
+//
+//  Created by Aryan Deore on 7/5/26.
+//
+
+import SwiftUI
+
+@main
+struct WatchLogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
